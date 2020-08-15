5c9e01d853070ec29d1f081ce47345a36b6b66f1
Daniel Vega <danielalexvega@gmail.com>
2020-08-15 15:04:10 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import CommentDetail from "./components/CommentDetail";
+
+const App = () => {
+  return (
+    <div className="ui container comments">
+      <CommentDetail />
+      <CommentDetail />
+      <CommentDetail />
+      
+     
+
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
